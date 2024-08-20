511e04e41d4a69badbe1673959e679bec74c7d9d
chenzhwsysu <ziweicen@icloud.com>
2024-08-20 10:06:03 +0800

    add auto run script

---
+++ auto_run.py
@@ -0,0 +1,25 @@
+# automatically run untils it ends normally.
+
+import subprocess
+import random
+import time 
+
+commands = [
+    # command1
+    [
+        "python", "bitbucket_with_check_progress_with_zip2jsonl_with_nontext_check.py",
+        "-u", "ziweicen",
+        "-p", "ATBBjAva5yDBvEycM8HY3vrLLFBq1ECDA449",
+        "-i", "clone_urls_1000_5000"
+    ]
+]
+
+for command in commands:
+    while True:
+        result = subprocess.run(command)
+        if result.returncode == 0:
+            print(f"\033[1;32mcommand {command} excuted successfully\033[0m", flush=True)
+            break
+        else:
+            print(f"\033[1;33mfailed {command}, retrying...\033[0m", flush=True)
+            time.sleep(random.randint(1,10))
