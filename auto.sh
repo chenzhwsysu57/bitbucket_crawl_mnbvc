f5e9f1367353e5ce5acd451e29977a9fa05fbfa0
chenzhwsysu <ziweicen@icloud.com>
2024-09-10 14:29:21 +0800

    rm -rf误删了😅项目重新下载中...

---
+++ auto.sh
@@ -0,0 +1,8 @@
+python auto_run.py --start      1 --end  50000 &
+python auto_run.py --start  50001 --end 100000 &
+python auto_run.py --start 100001 --end 150000 &
+python auto_run.py --start 150001 --end 200000 &
+python auto_run.py --start 200001 --end 250000 &
+python auto_run.py --start 250001 --end 300000 &
+python auto_run.py --start 300001 --end 350000 &
+python auto_run.py --start 350001 --end 400000 &
